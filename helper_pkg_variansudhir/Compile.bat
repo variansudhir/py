f1892e07be9076e1008aeadaf004bff13d67738d
Sudhir Srivastava <sudhir.kr84@gmail.com>
2018-08-29 15:13:36 +0530

    Fix incorrect file names

--- helper_pkg_variansudhir/Compile.bat
+++ helper_pkg_variansudhir/Compile.bat
@@ -1,5 +1,5 @@
 Rem Compile the code into a package
 python setup.py sdist bdist_wheel
 
-Rem Upload the package to https://test.pypi.org/project/example-pkg-variansudhir/
+Rem Upload the package to https://test.pypi.org/project/helper_pkg_variansudhir/
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
